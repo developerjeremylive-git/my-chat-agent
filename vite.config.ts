43e43373b520a0d4df0e75b681055a439fc88e05
Jeremy Live <codelive007@gmail.com>
2025-04-25 10:20:48 -0600

    update v2 - select

--- vite.config.ts
+++ vite.config.ts
@@ -11,4 +11,12 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    rollupOptions: {
+      external: [
+        'node:async_hooks',
+        'cloudflare:workers'
+      ]
+    }
+  }
 });
