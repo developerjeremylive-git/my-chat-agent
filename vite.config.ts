060e462c241542e78f21a27828f7c78b94a033a0
Jeremy Live <codelive007@gmail.com>
2025-04-25 10:32:08 -0600

    update v3 - select

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,11 @@ export default defineConfig({
         'node:async_hooks',
         'cloudflare:workers'
       ]
-    }
+    },
+    target: 'esnext',
+    modulePreload: false
+  },
+  optimizeDeps: {
+    exclude: ['node:async_hooks', 'cloudflare:workers']
   }
 });
