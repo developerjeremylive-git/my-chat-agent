0c2cccb52fbd14c66ed9d60259d881ec7eec581f
Jeremy Live <codelive007@gmail.com>
2025-04-25 06:44:27 -0600

    upgrade model to llama4

--- src/server.ts
+++ src/server.ts
@@ -15,7 +15,8 @@ import { tools, executions } from "./tools";
 import { AsyncLocalStorage } from "node:async_hooks";
 import { env } from "cloudflare:workers";
 const workersai = createWorkersAI({ binding: env.AI });
-const model = workersai("@cf/deepseek-ai/deepseek-r1-distill-qwen-32b")
+// const model = workersai("@cf/deepseek-ai/deepseek-r1-distill-qwen-32b");
+const model = workersai("@cf/meta/llama-4-scout-17b-16e-instruct");
 // Cloudflare AI Gateway
 // const openai = createOpenAI({
 //   apiKey: env.OPENAI_API_KEY,
