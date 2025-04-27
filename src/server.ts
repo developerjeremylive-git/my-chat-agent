c47b5927d51306824d83c2002651eb555f4f3bf2
Jeremy Live <codelive007@gmail.com>
2025-04-27 06:16:41 -0600

    streaming true

--- src/server.ts
+++ src/server.ts
@@ -79,6 +79,7 @@ export class Chat extends AIChatAgent<Env> {
             temperature: config.temperature,
             maxTokens: config.maxTokens,
             topP: config.topP,
+            toolCallStreaming: true,
             frequencyPenalty: config.frequencyPenalty,
             presencePenalty: config.presencePenalty,
             system: `You are a helpful assistant that can do various tasks... 
