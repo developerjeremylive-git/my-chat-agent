a8995314be500e3098568e4b3d5aea4ef8220453
Jeremy Live <codelive007@gmail.com>
2025-05-02 21:58:33 -0600

    upgrade v1

--- vite.config.ts
+++ vite.config.ts
@@ -11,9 +11,8 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  build: {
-    rollupOptions: {
-      external: ['stripe']
-    }
+  define: {
+    'process.env.VITE_SUPABASE_URL': JSON.stringify('https://enryddelkevswwawmkyx.supabase.co'),
+    'process.env.VITE_SUPABASE_KEY': JSON.stringify('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVucnlkZGVsa2V2c3d3YXdta3l4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDEzMDEwNzUsImV4cCI6MjA1Njg3NzA3NX0.qLMdHoQaD47thhpV-Eq3U0Hyr1fYDbygf0SKfGphKx0')
   }
 });
