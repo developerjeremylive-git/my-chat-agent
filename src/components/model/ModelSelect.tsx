2518528608b83c28639f34b98b8339dab2c35c29
Jeremy Live <codelive007@gmail.com>
2025-05-30 17:29:32 -0600

    altura menu modelos

--- src/components/model/ModelSelect.tsx
+++ src/components/model/ModelSelect.tsx
@@ -537,7 +537,7 @@ export function ModelSelect({ className, mobile = false }: ModelSelectProps) {
             style={{
               WebkitOverflowScrolling: 'touch',
               overscrollBehavior: 'contain',
-              maxHeight: mobile ? 'calc(100vh - 100px)' : '80vh',
+              maxHeight: mobile ? 'calc(53vh - 100px)' : '80vh',
               display: 'flex',
               flexDirection: 'column'
             }}
